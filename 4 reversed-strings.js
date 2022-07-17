2bc36bb69019354da05b05f65d1ac3428d5a5933
Alex <necrasoff.alex@yandex.ru>
2022-07-17 13:24:55 +0500

    4 tasks

---
+++ 4 reversed-strings.js
@@ -0,0 +1,11 @@
+// Завершите решение так, чтобы оно перевернуло переданную в него строку.
+
+// 'world'  =>  'dlrow'
+// 'word'   =>  'drow'
+
+
+
+
+function solution(str){
+    return str.split("").reverse().join("");
+}
